7637b0afcdd79bf337ac1f18352be3a9fd794d18
chris3ware <36608309+chris3ware@users.noreply.github.com>
2022-08-01 15:57:50 +0100

    chore(terraform): Split files into different directories

---
+++ terraform/vpc/providers.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  profile = "ans-gen" #* Need to create 'dev' profile 
+  region  = "us-east-1"
+
+  default_tags {
+    tags = {
+      "Project"     = "aws-network-specialty"
+      "Environment" = "general"
+      "Terraform"   = true
+    }
+  }
+}
+
+terraform {
+  required_version = ">= 1.2.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.71.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "3ware"
+    workspaces {
+      name = "aws-net-spec"
+    }
+  }
+}
