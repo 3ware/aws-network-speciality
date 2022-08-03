70978f5bf02ee83259f6d9a778238ad4e596185c
chris3ware <36608309+chris3ware@users.noreply.github.com>
2022-08-03 15:59:17 +0100

    feat(terraform): Add iam and ec2 resources to peering demo

--- terraform/vpc-peer/main.tf
+++ terraform/vpc-peer/main.tf
@@ -6,13 +6,13 @@ locals {
       description = "ipv4",
       protocol    = "tcp",
       port        = local.admin_port,
-      cidr_blocks = "0.0.0.0/0",
+      cidr_blocks = ["0.0.0.0/0"],
     },
     {
       description      = "ipv6",
       protocol         = "tcp",
       port             = local.admin_port,
-      ipv6_cidr_blocks = "::/0"
+      ipv6_cidr_blocks = ["::/0"]
 
     },
     #* Cannot use self due to bug in AWS provider
@@ -91,6 +91,8 @@ module "endpoints" {
   }
 }
 
+#* This combined with the stackoverflow link in locals gives a good description of this functionality
+#* https://dev.to/kernelpanicaus/safer-handling-of-resource-multiplicity-in-terraform-2ked
 resource "aws_security_group_rule" "ingress" {
   for_each = {
     for rule in local.ingress_rules_per_vpc : "${rule.description}-${rule.protocol}" => rule
@@ -99,20 +101,22 @@ resource "aws_security_group_rule" "ingress" {
   from_port         = lookup(each.value, "port")
   to_port           = lookup(each.value, "port")
   protocol          = lookup(each.value, "protocol")
-  cidr_blocks       = lookup(each.value, "cidr_block", [])
-  ipv6_cidr_blocks  = lookup(each.value, "ipv6_cidr_block", [])
+  cidr_blocks       = lookup(each.value, "cidr_blocks", [])
+  ipv6_cidr_blocks  = lookup(each.value, "ipv6_cidr_blocks", [])
   security_group_id = aws_security_group.this[each.value.vpc_name].id
 }
 
-# resource "aws_security_group_rule" "ingress_self" {
-#   for_each          = var.vpc
-#   type              = "ingress"
-#   from_port         = 0
-#   to_port           = 0
-#   protocol          = -1
-#   self              = true
-#   security_group_id = aws_security_group.this[each.key].id
-# }
+
+#* Seperate self rule required due to bug in AWS terraform provider. See link in locals
+resource "aws_security_group_rule" "self" {
+  for_each          = var.vpc
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = -1
+  self              = true
+  security_group_id = aws_security_group.this[each.key].id
+}
 
 resource "aws_security_group_rule" "egress" {
   for_each          = var.vpc
@@ -130,21 +134,45 @@ resource "aws_security_group" "this" {
   description = "Enable SSH access via port 22 IPv4 & v6"
   vpc_id      = module.vpc[each.key].vpc_id
 
-  tags = {
-    Name = "A4LPEERING-${local.vpc_short_name[each.key]}SecurityGroup-${random_string.random[each.key].result}"
-  }
+  #* Disabling this due to bug with dynamic content in tags
+  #* https://github.com/hashicorp/terraform-provider-aws/issues/19583#issuecomment-1125728202
+  # tags = {
+  #   Name = "A4LPEERING-${local.vpc_short_name[each.key]}SecurityGroup-${random_string.random[each.key].result}"
+  # }
 }
 
-# #! tfsec:ignore:aws-ec2-enable-at-rest-encryption #! tfsec:ignore:aws-ec2-enforce-http-token-imds
-# resource "aws_instance" "a4l_bastion" {
-#   ami                         = "ami-033b95fb8079dc481"
-#   instance_type               = "t2.micro"
-#   subnet_id                   = module.vpc.public_subnets[0]
-#   associate_public_ip_address = true
-#   vpc_security_group_ids      = [aws_security_group.bastion.id]
-#   key_name                    = aws_key_pair.a4l.key_name
+module "iam_assumable_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = "~>5.2.0"
 
-#   tags = {
-#     Name = "A4L-BASTION"
-#   }
-# }
+  role_name               = "A4LPEERING-EC2Role"
+  create_role             = true
+  create_instance_profile = true
+  role_requires_mfa       = false
+
+  trusted_role_services = [
+    "ec2.amazonaws.com"
+  ]
+}
+
+#* Inline policy not currently supported in the iam_assumable_role module so using seperate resource for now
+#* https://github.com/terraform-aws-modules/terraform-aws-iam/issues/261
+resource "aws_iam_role_policy" "inline_policy" {
+  name   = "inline_policy"
+  role   = module.iam_assumable_role.iam_role_name
+  policy = data.aws_iam_policy_document.ec2_role.json
+}
+
+#! tfsec:ignore:aws-ec2-enable-at-rest-encryption #! tfsec:ignore:aws-ec2-enforce-http-token-imds
+resource "aws_instance" "a4l_peering" {
+  for_each               = var.vpc
+  ami                    = "ami-0cabc39acf991f4f1"
+  instance_type          = "t2.micro"
+  subnet_id              = module.vpc[each.key].private_subnets[0]
+  vpc_security_group_ids = [aws_security_group.this[each.key].id]
+  iam_instance_profile   = module.iam_assumable_role.iam_instance_profile_name
+
+  tags = {
+    Name = "A4L-EC2${local.vpc_short_name[each.key]}"
+  }
+}
