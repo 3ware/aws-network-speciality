15fd276f1f9fab2551d485dfb1896c8d815fc1fa
chris3ware <36608309+chris3ware@users.noreply.github.com>
2025-04-07 15:02:14 +0100

    feat(iac): Add variable validation to vpc module

--- iac/modules/vpc/variables.tf
+++ iac/modules/vpc/variables.tf
@@ -1,12 +1,33 @@
 variable "aws_environment" {
-  description = "(Required) The AWS environment to deploy resources to"
+  description = <<EOT
+    (Required) The AWS environment to deploy resources to.
+    Valid values are: development, testing, staging, production.
+    EOT
   type        = string
   nullable    = false
+  validation {
+    condition = contains(["development", "testing", "staging", "production"], var.aws_environment)
+    error_message = format(
+      "Invalid environment provided. Received: '%s', Require: '%v'.\n%s",
+      var.aws_environment,
+      join(", ", ["development", "testing", "staging", "production"]),
+      "Change the environment variable value to one that is permitted."
+    )
+  }
 }
 
 variable "vpc_cidr_block" {
-  description = "(Required) The CIDR block for the VPC"
+  description = "(Required) A valid CIDR block to assign to the VPC"
   type        = string
+
+  validation {
+    condition = can(cidrhost(var.vpc_cidr_block, 0))
+    error_message = format(
+      "Invalid CIDR block provided. Received: '%s'\n%s",
+      var.vpc_cidr_block,
+      "Check the syntax of the CIDR block is valid."
+    )
+  }
 }
 
 variable "trusted_ips" {
